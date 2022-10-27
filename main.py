2173ab1baa6f4d23c9847bcebfb794e1888664ac
MichaelZhangJiahao <michaelzjh2002@gmail.com>
2022-10-27 20:04:51 +0800

    simplify go

--- main.py
+++ main.py
@@ -28,29 +28,13 @@ class AI(object):
         # The system will get the end of your candidate_list as your decision.
         self.candidate_list = []
 
-    # The input is the current chessboard. Chessboard is a numpy array.
     def go(self, chessboard):
         self.candidate_list.clear()
         for y, x in np.ndindex(chessboard.shape):
-            if chessboard[y][x] == 0:
-                flag = False
-                neighbour_enemy = self.check_neighbour(y, x, chessboard)
-                for direction in neighbour_enemy:
-                    yi = direction[0]
-                    xi = direction[1]
-                    y_cur = y + yi
-                    x_cur = x + xi
-                    while self.not_out_of_bound(y_cur, x_cur):
-                        y_cur += yi
-                        x_cur += xi
-                        if self.not_out_of_bound(y_cur, x_cur):
-                            if chessboard[y_cur][x_cur] == self_color:
-                                flag = True
-                            elif chessboard[y_cur][x_cur] == 0:
-                                break
-                if flag:
-                    self.candidate_list.append((y, x))
+            if self.is_legal(chessboard, y, x):
+                self.candidate_list.append((y, x))
 
+    # check whether it has opponent neighbour, and return a direction
     def check_neighbour(self, y, x, board):
         neighbour_enemy_direction = []
         if self.not_out_of_bound(y - 1, x):
@@ -80,7 +64,54 @@ class AI(object):
 
         return neighbour_enemy_direction
 
+    # check whether(x, y) is out of bound
     def not_out_of_bound(self, y, x):
         if (0 <= y <= self.chessboard_size - 1) and (0 <= x <= self.chessboard_size - 1):
             return True
         return False
+
+    def update_board(self, chessboard, y, x):   #(y, x) is a solution, but not sure what direction
+        updated_chessboard = chessboard.copy()
+        neighbour_enemy = self.check_neighbour(y, x, updated_chessboard)
+        for direction in neighbour_enemy:
+            flag = False
+            yi = direction[0]
+            xi = direction[1]
+            y_cur = y + yi
+            x_cur = x + xi
+            while self.not_out_of_bound(y_cur, x_cur):
+                y_cur += yi
+                x_cur += xi
+                if self.not_out_of_bound(y_cur, x_cur):
+                    if updated_chessboard[y_cur][x_cur] == self_color:     #不可能紧挨着，因为是enemy direction
+                        flag = True
+                        break
+                    elif updated_chessboard[y_cur][x_cur] == 0:
+                        break
+            if flag:
+                    y_temp, x_temp = y, x
+                    while(y_temp != y_cur):
+                        y_temp = y + yi
+                        x_temp = x + xi
+                        updated_chessboard[y_temp][x_temp] = self_color
+        return updated_chessboard
+    
+    # judge whether (y,x) is legal
+    def is_legal(self, chessboard, y, x):
+        flag = False
+        if chessboard[y][x] == 0:
+            neighbour_enemy = self.check_neighbour(y, x, chessboard)
+            for direction in neighbour_enemy:
+                yi = direction[0]
+                xi = direction[1]
+                y_cur = y + yi
+                x_cur = x + xi
+                while self.not_out_of_bound(y_cur, x_cur):
+                    y_cur += yi
+                    x_cur += xi
+                    if self.not_out_of_bound(y_cur, x_cur):
+                        if chessboard[y_cur][x_cur] == self_color:
+                            flag = True
+                        elif chessboard[y_cur][x_cur] == 0:
+                            break
+        return flag
